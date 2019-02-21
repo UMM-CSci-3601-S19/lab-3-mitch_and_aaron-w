c2563fa949e0e85811012a5c6b6bfd77b66439a1
walte774 <walte774@morris.umn.edu>
2019-02-21 14:31:01 -0600

    Add more functionality to todos
      Uses user-list.component, user-list.service, and user.component as templates

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,11 +8,14 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from './users/user-list.component';
 import {UserListService} from './users/user-list.service';
+import {TodoListComponent} from './todos/todo-list.component';
+import {TodoListService} from './todos/todo-list.service';
 import {Routing} from './app.routes';
 import {APP_BASE_HREF} from '@angular/common';
 
 import {CustomModule} from './custom.module';
 import {UserComponent} from './users/user.component';
+import {TodoComponent} from './todos/todo.component';
 
 
 @NgModule({
@@ -27,10 +30,13 @@ import {UserComponent} from './users/user.component';
     HomeComponent,
     UserListComponent,
     UserComponent,
+    TodoListComponent,
+    TodoComponent,
 
   ],
   providers: [
     UserListService,
+    TodoListService,
     {provide: APP_BASE_HREF, useValue: '/'},
     {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}
   ],
